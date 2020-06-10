0ed4915cee6d2e9c7ef920d0aa20eaaa7cdc77f5
Bibin <bibin3@gmail.com>
2020-06-10 18:45:05 +0530

    ck editor

--- src/app/core/components/views/basic/basic.component.ts
+++ src/app/core/components/views/basic/basic.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-basic',
