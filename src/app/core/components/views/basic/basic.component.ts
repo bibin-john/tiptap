1d80f4100b8aca2cd7bbb36571857b730666b203
Bibin <bibin3@gmail.com>
2020-06-10 18:36:04 +0530

    editor added

--- src/app/core/components/views/basic/basic.component.ts
+++ src/app/core/components/views/basic/basic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 @Component({
   selector: 'app-basic',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicComponent implements OnInit {
 
+  public Editor = ClassicEditor;
   constructor() { }
 
   ngOnInit() {
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,14 @@
- import { NgModule } from '@angular/core';
- import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BasicComponent } from './components/views/basic/basic.component';
 import { MenuBubbleComponent } from './components/views/menu-bubble/menu-bubble.component';
 import { FloatingMenuComponent } from './components/views/floating-menu/floating-menu.component';
-
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 @NgModule({
   declarations: [ BasicComponent, MenuBubbleComponent, FloatingMenuComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CKEditorModule
   ],
   exports: [],
   entryComponents: [],
